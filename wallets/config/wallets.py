883fba9b65671febf2dbf3ceb1969eac263ab2d3
Sebastian Hutter <mail@sebastian-hutter.ch>
2017-08-15 22:09:08 +0200

    sebastian: add basic config validation

--- wallets/config/wallets.py
+++ wallets/config/wallets.py
@@ -12,8 +12,24 @@ class WalletsConfiguration(object):
     def __init__(self):
         """ initialize config """
         self.global_settings = {}
+        # sqlite database gfile
         self.global_settings['database'] = os.getenv('WALLETS_DATABASE','wallets.db')
-        self.global_settings['schedule'] = os.getenv('WALLETS_SCHEDULE',None)
+        # set to 0 to disable scheduled check of the wallets
+        self.global_settings['schedule'] = os.getenv('WALLETS_SCHEDULE',0)
+        # eecute a wallet scan at statup of the script, set to true / false
+        self.global_settings['scanonstart'] = os.getenv('WALLETS_SCAN_ON_START',False)
+        # set to true to enable webinterface for easier browsing
+        self.global_settings['website'] = os.getenv('WALLETS_WEBSITE',False)
+
+        # verify valuess
+        if str(self.global_settings['scanonstart']).lower() in ['true', '1', 't', 'y', 'yes' ]:
+            self.global_settings['scanonstart'] = True
+        else:
+            self.global_settings['scanonstart'] = False
+        if str(self.global_settings['website']).lower() in ['true', '1', 't', 'y', 'yes' ]:
+            self.global_settings['website'] = True
+        else:
+            self.global_settings['website'] = False
 
         # load exchanges
         self.exchanges = {}
